e9e8dd4dd2d05618284d613bb158a74a3a614ea8
chaos0103 <lyt1228@naver.com>
2023-06-08 00:23:36 +0900

    🧙feat(member): AH-12 로그인 기능 구현

--- src/main/java/accio/hogsmeade/store/client/member/service/impl/MemberAccountServiceImpl.java
+++ src/main/java/accio/hogsmeade/store/client/member/service/impl/MemberAccountServiceImpl.java
@@ -1,16 +1,32 @@
 package accio.hogsmeade.store.client.member.service.impl;
 
 import accio.hogsmeade.store.client.member.service.MemberAccountService;
+import accio.hogsmeade.store.jwt.JwtTokenProvider;
 import accio.hogsmeade.store.jwt.TokenInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberAccountServiceImpl implements MemberAccountService {
 
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     public TokenInfo login(String loginId, String loginPw) {
-        return null;
+        //loginId, loginPw를 기반으로 Authentication 객체 생성
+        //authentication은 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginId, loginPw);
+
+        //실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
+        //authenticate 메서드가 실행될 때 CustomUserDetailsService에서 만든 loadUserByUsername 메서드가 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        //인증 정보를 기반으로 JWT 토큰 생성
+        return jwtTokenProvider.generateToken(authentication);
     }
 }
