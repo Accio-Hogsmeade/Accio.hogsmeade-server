8aab1eb75a92ca982202d7f2169a365b446297ea
chaos0103 <lyt1228@naver.com>
2023-06-07 13:36:07 +0900

    🧙feat(member): AH-11 회원탈퇴 api 컨트롤러 구현

--- src/main/java/accio/hogsmeade/store/client/api/AccountApiController.java
+++ src/main/java/accio/hogsmeade/store/client/api/AccountApiController.java
@@ -1,15 +1,19 @@
 package accio.hogsmeade.store.client.api;
 
 import accio.hogsmeade.store.client.api.request.member.SignupMemberRequest;
+import accio.hogsmeade.store.client.api.request.member.WithdrawalMemberRequest;
 import accio.hogsmeade.store.client.member.service.MemberService;
 import accio.hogsmeade.store.client.member.service.dto.SignupMemberDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @Slf4j
@@ -38,6 +42,16 @@ public class AccountApiController {
 
         Long memberId = memberService.signup(dto);
         log.info("signup member={}", memberId);
+        return memberId;
+    }
+
+    @ApiOperation(value = "회원탈퇴")
+    @DeleteMapping("/withdrawal")
+    public Long withdrawal(@Valid @RequestBody WithdrawalMemberRequest request) {
+        log.debug("WithdrawalMemberRequest={}", request);
+        Long memberId = memberService.withdrawal(request.getLoginId(), request.getLoginPw());
+
+        log.info("withdrawal member={}", memberId);
         return memberId;
     }
 }
