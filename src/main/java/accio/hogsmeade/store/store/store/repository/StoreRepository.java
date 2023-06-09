87d571082176ebece7bf6ed822715a1322f45d23
leeyr0412 <leeyr0412@naver.com>
2023-06-09 20:42:05 +0900

    🧙feat(store): AH-6 StoreRepository 생성

---
+++ src/main/java/accio/hogsmeade/store/store/store/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package accio.hogsmeade.store.store.store.repository;
+
+import accio.hogsmeade.store.store.store.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+}
