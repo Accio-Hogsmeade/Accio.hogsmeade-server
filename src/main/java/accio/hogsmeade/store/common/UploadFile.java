da4ba04c1158b6e46448f3bcebe994acc56dcdd6
chaos0103 <lyt1228@naver.com>
2023-06-13 17:22:51 +0900

    🧙feat(board): AH-16 파일 저장 기능 구현

---
+++ src/main/java/accio/hogsmeade/store/common/UploadFile.java
@@ -0,0 +1,24 @@
+package accio.hogsmeade.store.common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class UploadFile {
+
+    private String uploadFileName;
+    private String storeFileName;
+
+    @Builder
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+}
