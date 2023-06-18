5afe71cc07a43abc575b440d34a092c730688963
chaos0103 <lyt1228@naver.com>
2023-06-18 22:19:03 +0900

    🧙feat(board): AH-25 BoardReport 도메인 설계

---
+++ src/main/java/accio/hogsmeade/store/client/board/BoardReport.java
@@ -0,0 +1,39 @@
+package accio.hogsmeade.store.client.board;
+
+import accio.hogsmeade.store.client.member.Member;
+import accio.hogsmeade.store.common.TimeBaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class BoardReport extends TimeBaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "board_report_id")
+    private Long id;
+    @Column(nullable = false, updatable = false, length = 100)
+    private String reason;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Builder
+    public BoardReport(Long id, String reason, Member member, Board board) {
+        this.id = id;
+        this.reason = reason;
+        this.member = member;
+        this.board = board;
+    }
+}
